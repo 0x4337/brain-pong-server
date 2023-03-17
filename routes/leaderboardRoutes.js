aa29c9adf86b32dc8bced337cecb5bd500a3c035
Vantria <s1spam118@gmail.com>
2023-03-17 11:58:39 +0000

    add winrate + score

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -20,4 +20,115 @@ router.get("/", (req, res) => {
   });
 });
 
+// increment games won by 1
+router.patch("/:playerId/win", (req, res) => {
+  fs.readFile("./data/players.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "cant read file",
+      });
+    }
+
+    const players = JSON.parse(data);
+    const playerId = req.params.playerId;
+    const player = players.find((player) => player.id === playerId);
+
+    if (!player) {
+      return res.status(404).json({
+        error: true,
+        message: "player not found",
+      });
+    }
+
+    player.gamesWon += 1;
+    player.gamesPlayed += 1;
+
+    fs.writeFile("./data/players.json", JSON.stringify(players), (err) => {
+      if (err) {
+        return res.status(500).json({
+          error: true,
+          message: "cant write file",
+        });
+      }
+
+      res.json(player);
+    });
+  });
+});
+
+router.patch("/:playerId/lost", (req, res) => {
+  fs.readFile("./data/players.json", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "cant read file",
+      });
+    }
+
+    const players = JSON.parse(data);
+    const playerId = req.params.playerId;
+    const player = players.find((player) => player.id === playerId);
+
+    if (!player) {
+      return res.status(404).json({
+        error: true,
+        message: "player not found",
+      });
+    }
+
+    player.gamesPlayed += 1;
+
+    fs.writeFile("./data/players.json", JSON.stringify(players), (err) => {
+      if (err) {
+        return res.status(500).json({
+          error: true,
+          message: "cant write file",
+        });
+      }
+
+      res.json(player);
+    });
+  });
+});
+
+// router.patch("/score", (req, res) => {
+//   fs.readFile("./data/players.json", (err, data) => {
+//     if (err) {
+//       return res.status(500).json({
+//         error: true,
+//         message: "cant read file",
+//       });
+//     }
+
+//     const players = JSON.parse(data);
+
+//     // console.log(players);
+
+//     const playerScore = req.body.score;
+
+//     if (!players) {
+//       return res.status(404).json({
+//         error: true,
+//         message: "players not found",
+//       });
+//     }
+
+//     players.forEach((player) => {
+//       player.score = playerScore;
+//     });
+
+//     // fs.writeFile("./data/players.json", JSON.stringify(players), (err) => {
+//     //   if (err) {
+//     //     return res.status(500).json({
+//     //       error: true,
+//     //       message: "cant write file",
+//     //     });
+//     //   }
+
+//     //   res.json(players);
+//     // });
+//   });
+// });
+
 module.exports = router;
